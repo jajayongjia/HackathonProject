010b157cbd03f10b25ece5e8842cd7c9182bfc78
Zuofu Li <zuofu@Zuofus-MacBook-Pro.local>
2019-09-07 18:39:57 -0600

    completed react framework

---
+++ mysite/my-app/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import TableData from './table/table.js'
+
+
+function App() {
+  return (
+
+    <div className="App">
+      <TableData></TableData>
+
+    </div>
+  );
+}
+
+export default App;

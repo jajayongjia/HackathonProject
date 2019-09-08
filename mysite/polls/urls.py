059a3e45ba24ee66c09cd19d627d93a138cd507d
haotianzhu <zhuhaotian19970215@gmail.com>
2019-09-07 18:22:52 -0600

    add back-end data api

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('data', views.get_all_data, name='fetch all'),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
+from  .models import Ranking
+import json
 
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+def get_all_data(request):
+    if request.method == 'GET':
+        data = {"data": []}
+        qs = Ranking.objects.all()
+        for one_rank in qs:
+            data['data'].append({
+                "id": one_rank.id,
+                "yearRange": one_rank.yearRange,
+                "localtion": one_rank.location,
+                "studentType": one_rank.studentType,
+                "tuitionFee": one_rank.tuitionFee
+                })
+        return HttpResponse(status=200, content=json.dumps(data), content_type='application/json')
+    else:
+        return HttpResponse(status=405)
